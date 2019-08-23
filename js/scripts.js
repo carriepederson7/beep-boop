4d0d30409200d0c39268c474534a9f889db578b7
Carrie Schmid <carriepederson7@gmail.com>
2019-08-23 16:12:12 -0700

    Add for loop to project.

--- js/scripts.js
+++ js/scripts.js
@@ -16,28 +16,35 @@ $(document).ready(function(){
 
 function findThrees(input) {
   var numbers = input.split("");
-  var threes = [];
-  var output = [];
-  // var sorryDave = "I'm sorry Dave, I can't do that."
-  // var beep = "beep"
-  // var boop = "boop"
-  console.log(numbers);
-  console.log(threes);
 
-  numbers.forEach(number){
-    if (number === "3"){
-      return "sorryDave";
-    }else if (number === "2" ){
-      return "beep";
-    }else if (number === "1"){
-      return "boop";
-    }else{
-      return numbers;
-    };
+  for(var i=0; i<numbers.length; i++) {
+    if(numbers[i]=== "3"){
+      return "Sorry Dave."
+    }else if (numbers[i]==="2"){
+      return "beep"
+    }else if (numbers[i] === "1")
+    return "boop"
+  }else {
+    return numbers;
+  }
+}
+}
 
+
+  // numbers.forEach(number){
+  //   if (number === "3"){
+  //     return "sorryDave";
+  //   }else if (number === "2" ){
+  //     return "beep";
+  //   }else if (number === "1"){
+  //     return "boop";
+  //   }else{
+  //     return numbers;
+  //   };
   //
-    }
-  //     return
-    }
-
+  // //
+  //   }
+  // //     return
+  //   }
+  //
   //
