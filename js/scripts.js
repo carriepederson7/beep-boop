bad7c5e4fa7f81509dad6e07a8ceee9a4de6878b
Carrie Schmid <carriepederson7@gmail.com>
2019-08-23 12:16:43 -0700

    Add findThrees function to project.

--- js/scripts.js
+++ js/scripts.js
@@ -5,8 +5,25 @@ $(document).ready(function(){
   $("#beep-boop").submit(function(event){
   event.preventDefault();
   var input = $("#inputNumber").val();
+  var result = findThrees(input);
   $("#result").append(input);
 
 
 });
 });
+
+//Business Logic
+
+function findThrees(input) {
+  var newArray = parseInt(input);
+  var threes = [];
+
+  for(var i= 0; i>=newArray.length; i++){
+    if (newArray[i] === "3"){
+      threes.push(newArray[i]);
+    } else{
+      return
+    }
+  }
+}
+// alert("ranloop");
