9de23ed3facfd61869695ce6e522e596537d45b0
Carrie Schmid <carriepederson7@gmail.com>
2019-08-24 20:04:50 -0700

    Add textResponse array loop.

--- js/scripts.js
+++ js/scripts.js
@@ -17,21 +17,39 @@ $(document).ready(function(){
 function findThrees(input) {
   var numbers = input.split("");
   console.log(numbers);
+  var textResponse = [];
   var output = [];
 
   for(var i=0; i<numbers.length; i++) {
     if(numbers[i]=== "3"){
-      output.push("Sorry Dave.");
+      textResponse.push("Sorry Dave");
     }else if (numbers[i]==="2"){
-      output.push("Beep.");
+      textResponse.push("Boop");
     }else if (numbers[i] === "1"){
-      output.push("Boop.");
+      textResponse.push("Beep");
     }else {
-      output.push(numbers[i]);
+      // output.push(numbers[i]);
     }
-    console.log(output);
   }
-  return output;
+
+  for (var i=0; i<textResponse.length; i++){
+    if(textResponse[i] === "Sorry Dave"){
+      output.push("I'm sorry Dave.")
+    }else if (textResponse[i] === "Boop"){
+      output.push("Boop.")
+    }else if (textResponse[i] === "Beep"){
+      output.push("Beep.")
+    }else{
+    }
+
+    console.log(textResponse);
+    console.log(output);
+    return output
+
+  }
+
+
+
 }
 
 
